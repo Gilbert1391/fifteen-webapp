3b905321f854d30740f169d3ee9976f458fc12f2
Gilbert Rosario <pro.devgilbert@gmail.com>
2019-01-06 15:04:41 -0400

    Implemented not found page

--- fifteen-ui/src/components/routes/notFound.jsx
+++ fifteen-ui/src/components/routes/notFound.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
 
 const NotFound = () => {
-  return <div>Not Found</div>;
+  return (
+    <div className="error-page">
+      <h1 className="error-page__heading">404 Error :(</h1>
+      <p className="error-page__paragraph">Page Not Found</p>
+      <Link to="/" className="error-page__link">
+        Go back to the front page{" "}
+        <FontAwesomeIcon
+          icon={faLongArrowAltRight}
+          className="error-page__icon"
+        />
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
